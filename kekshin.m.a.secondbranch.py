02a74ca506774c2844f2523099f079dccf824a9d
KekshinMaks <kekshin.maksim@gmail.com>
2024-09-23 16:54:24 +0300

    thirdtask

--- kekshin.m.a.secondbranch.py
+++ kekshin.m.a.secondbranch.py
@@ -3,8 +3,18 @@ a = int(input())
 print("Введи второе число: ")
 b = int(input())
 
-if a > b:
-    print("good")
+def f(x):
+    if f(x) >= 0:
+        return 1
+    else:
+        return 0
+    
+if f(a) == 1:
+    if a > b:
+        print("good")
+    else:
+        print("bad")
 else:
-    print("bad")
+    print("Sorry stop push this numbers")
+
 
